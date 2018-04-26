304c9b067be89fa324e5a17634d9152768c4a104
FixyNeko <felix@dhumes.fr>
2018-04-26 22:19:25 +0200

    working user login

--- Web/www/js/connect.js
+++ Web/www/js/connect.js
@@ -1,24 +1,21 @@
-function initConnect(){
-    var login = document.getElementById('login');
-    var password = document.getElementById('password');
-    if(login.value.length > 0 && password.value.length > 0){
-        var xhr = new XMLHttpRequest();
-        var form = new FormData();
-        form.append('username', login.value);
-        form.append('userpwd', password.value);
+document.getElementById('connexionForm').addEventListener('submit', function(e){
+    var username = document.forms['connect'].elements["username"].value;
+    var userpwd = document.forms['connect'].elements["userpwd"].value;
+    var xhr = new XMLHttpRequest();
+    var form = new FormData();
+    if(username.length > 0) form.append('username', username);
+    if(userpwd.length  > 0) form.append('userpwd', userpwd);
 
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                var newWindow = window.open();
-                newWindow.document.write(xhr.responseText);
-            }
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            document.forms['connect'].elements["username"].value = "";
+            document.forms['connect'].elements["userpwd"].value = "";
+            document.getElementById("greetingMessage").textContent = xhr.responseText;
         }
-
-
-        xhr.open('POST', '../htbin/login.py');
-        xhr.send(form);
     }
-    else {
 
-    }
-}
+    xhr.open('POST', '../htbin/login.py');
+    xhr.send(form);
+
+    e.preventDefault();
+});
