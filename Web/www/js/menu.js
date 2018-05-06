c3b2b764b281b35a7ac72fe9d76ee91016747606
Clement_Dif <clementdt99@gmail.com>
2018-05-06 16:42:24 +0200

    Conversion des indentations en tabulations.

--- Web/www/js/menu.js
+++ Web/www/js/menu.js
@@ -1,8 +1,8 @@
 function displayMenu() {
-    var x = document.getElementById("menu_id");
-    if (x.className === "menu_class") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu_class";
-    }
+	var x = document.getElementById("menu_id");
+	if (x.className === "menu_class") {
+		x.className += " responsive";
+	} else {
+		x.className = "menu_class";
+	}
 }
