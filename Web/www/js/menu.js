8867cbe8026b596e79211c1f4cbe52d791a767c9
Clement_Dif <clementdt99@gmail.com>
2018-05-26 00:28:30 +0200

    Mise en forme, diverses améliorations, correction des fautes.

--- Web/www/js/menu.js
+++ Web/www/js/menu.js
@@ -1,11 +1,11 @@
-function displayMenu() {
+document.getElementById('menu_icon').addEventListener('click', function(e) {
 	var x = document.getElementById("menu_id");
 	if (x.className === "menu_class") {
 		x.className += " responsive";
 	} else {
 		x.className = "menu_class";
 	}
-}
+});
 
 document.getElementById('chatbox').addEventListener('click', function(e){
 	var x = document.getElementById("chatbox");
